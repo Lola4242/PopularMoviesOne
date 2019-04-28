af5f0db640481cb3a0af1225a4a515e0a3bddc2b
Meeuwenberg, Y.W.A. (Ylona) <ylona.meeuwenberg@devolksbank.nl>
2019-04-28 15:22:24 +0200

    Simple static movie list

--- app/src/main/java/android/example/popularmoviesone/MainActivity.java
+++ app/src/main/java/android/example/popularmoviesone/MainActivity.java
@@ -2,12 +2,27 @@ package android.example.popularmoviesone;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //TODO change recycler view
+        String[] movies = getResources().getStringArray(R.array.mock_data);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1, movies);
+
+        // Simplification: Using a ListView instead of a RecyclerView
+        ListView listView = findViewById(R.id.recyclerview_movies);
+        listView.setAdapter(adapter);
+
     }
+
+
 }
